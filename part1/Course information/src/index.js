98d8e369eb4f2b120fe6f593489b7097e062350d
Sevastiyan <sevi.tsv@gmail.com>
2020-03-17 17:31:04 +0800

    added exercise 1.5 course information step5

--- part1/Course information/src/index.js
+++ part1/Course information/src/index.js
@@ -23,38 +23,39 @@ const Part = (props) => {
 	)
 }
 
+const sum = function(array){
+	return array[0].exercises + array[1].exercises + array[2].exercises
+}
+
 const Total = (props) => {
 	return(
-		<p>Number of exercises {
-			props.parts[0].exercises + 
-			props.parts[1].exercises + 
-			props.parts[2].exercises
-		}</p>
+		<p>Number of exercises {sum(props.parts)}</p>
 	)
 }
 
 const App = () => {
-	const course = 'Half Stack application development'
-	
-	const parts = [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7
-		},
-		{ 
-			name: 'State of a component',
-			exercises: 14
-		}
-	]
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7
+			},
+			{ 
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
   return (
     <div>
-			<Header course={course} />
-			<Content parts={parts}/>
-			<Total parts={parts}/>
+			<Header course={course.name} />
+			<Content parts={course.parts}/>
+			<Total parts={course.parts}/>
     </div>
   )
 }

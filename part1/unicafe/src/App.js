d6e91c6c07f85cf7097bba40dad954461e0cffee
webstah <sevastiyan.tsv@gmail.com>
2022-04-28 14:17:24 +0900

    part1/unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Header = () => <h1>Give Feedback</h1>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const Statistics = ({ good, neutral, bad }) => {
+  const sum = good + neutral + bad;
+  if (sum === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  } else {
+    const average = (good - bad) / sum;
+    const positive = (good / sum) * 100;
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='all' value={sum} />
+            <StatisticLine text='average' value={average} />
+            <StatisticLine text='positive' value={positive} />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+const StatisticLine = ({ text, value }) => {
+  if (text === 'positive') {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  console.log(good, neutral, bad);
+
+  return (
+    <div>
+      <Header />
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
 
 export default App;
